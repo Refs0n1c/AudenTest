5b273d4fabcff2eacc2a31be23ce663c60547ef6
Refs0n1c <daniel.refson2306@gmail.com>
2022-11-23 22:47:05 +0000

    brackets

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ class Checkout:
     recordByItemSku = self.checkoutData.query(f"ItemSku=='{itemSku}'")
     itemsIncludedInDiscount = qty / recordByItemSku['SpecialItemQty'].values[0]
     itemsAtFullPrice = qty % recordByItemSku['SpecialItemQty'].values[0]
-    total = round(itemsIncludedInDiscount) * recordByItemSku['SpecialItemsCosts'].values[0] 
+    total = (round(itemsIncludedInDiscount) * recordByItemSku['SpecialItemsCosts'].values[0]) 
     + (round(itemsAtFullPrice) * recordByItemSku['ItemPrice'].values[0])
     return total
     
